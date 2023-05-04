cb9d5bd37078f15737e10249c831374c6b5eba83
nikhil08062002 <129737306+nikhil08062002@users.noreply.github.com>
2023-05-04 14:13:42 +0530

    Add files via upload

---
+++ Task 2/survy form.js
@@ -0,0 +1,5 @@
+function validateForm() 
+	// Get form inputs
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var phone = document.getElementById("phone").value;
